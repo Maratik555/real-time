4cb153d117dcb357f2412e79a3a68cc189a01e2a
Маратик555 <amr404@bk.ru>
2022-04-10 23:08:35 +0300

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './app.css'
+import LongPulling from './LongPulling'
+import Eventscr from './Eventscr'
+import WebSocket from './WebSocket'
+
+function App() {
+  return (
+    <div>
+      <WebSocket/>
+    </div>
+  )
+}
+
+export default App
---
+++ server/long-pulling.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors = require('cors')
+const events = require('events')
+
+const PORT = 7777
+
+const emitter = new events.EventEmitter()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/get-messages', (req, res) => {
+  emitter.once('newMessage', (message) => {
+    res.json(message)
+  })
+})
+
+app.post('/new-messages', ((req, res) => {
+  const message = req.body
+  emitter.emit('newMessage', message)
+  res.status(200)
+}))
+
+
+app.listen(PORT, (err) => {
+  if (err) {
+    throw Error(err)
+  }
+  console.log(`server started on port ${PORT}`)
+})
